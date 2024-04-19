74c9c8348f1644d7cd7d4451612624b802cf0eb3
Stanislav <stanislav.panishev@yandex.ru>
2024-04-19 14:36:56 +0600

    Sprint 9. Review 1

--- src/http/adapters/DurationAdapter.java
+++ src/http/adapters/DurationAdapter.java
@@ -4,13 +4,14 @@ import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
+
 import java.io.IOException;
 import java.time.Duration;
 
 public class DurationAdapter extends TypeAdapter<Duration> {
     @Override
     public void write(JsonWriter jsonWriter, Duration duration) throws IOException {
-        if(duration != null){
+        if (duration != null) {
             jsonWriter.value(duration.toMinutes());
         } else {
             jsonWriter.nullValue();
--- src/http/adapters/LocalDateTimeAdapter.java
+++ src/http/adapters/LocalDateTimeAdapter.java
@@ -4,6 +4,7 @@ import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
+
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -11,9 +12,10 @@ import java.time.format.DateTimeFormatter;
 public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+
     @Override
     public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
-        if (localDateTime == null)  {
+        if (localDateTime == null) {
             jsonWriter.nullValue();
         } else {
             jsonWriter.value(localDateTime.format(formatter));
